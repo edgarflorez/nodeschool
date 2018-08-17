ea5e997ff9620e266d894711add01fc4da5a426e
edgarflorez <edgarfmail@gmail.com>
2018-08-16 22:54:44 -0500

    Functional * hello world challenge

---
+++ functional-javascript/01-hello-world.js
@@ -0,0 +1,17 @@
+function upperCaser(input) {
+    return input.toUpperCase();
+}
+module.exports = upperCaser
+
+
+
+
+
+
+
+/*
+// Official solution
+module.exports = function(input) {      
+    return input.toUpperCase()
+}
+*/
