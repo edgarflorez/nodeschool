7b0045cb14ecc36cd7fce682dc0edab6906711e7
Edgar Florez <edgarflorez@MacBook-Pro-de-Edgar.local>
2018-08-16 22:37:09 -0500

    Learnyounode * http-json-api-server

--- learnyounode/13-http-json-api-server.js
+++ learnyounode/13-http-json-api-server.js
@@ -4,32 +4,73 @@ const url = require('url');
 const port = process.argv[2];
 
 const server = http.createServer( function (request, response) {
-    const urlObject = url.parse( request.url, true );
-
-    //console.log(urlObject);
-
+    
     if(request.method !== 'GET'){
         return response.end('send me a GET\n')
     }
+    
+    const urlObject = url.parse( request.url, true );
+    const queryDate = new Date(urlObject.query.iso);
+    let objDate = {}
 
-    response.writeHead(200, { 'Content-Type': 'application/json' })
+    response.writeHead(200, { 'Content-Type': 'application/json' });
 
     switch (urlObject.pathname) {
         case '/api/parsetime':
-            console.log(urlObject);
+            objDate.hour = queryDate.getHours();
+            objDate.minute = queryDate.getMinutes();
+            objDate.second = queryDate.getSeconds();
             break;
             
         case '/api/unixtime':
-            console.log(urlObject);
+            objDate.unixtime = queryDate.getTime();
             break;
     
         default:
             break;
     }
 
-    response.end();
-
-
+    response.end( JSON.stringify(objDate));
 
 } )
 server.listen(port);
+
+
+
+
+
+
+
+/*
+// Official solution
+var http = require('http');
+var url = require('url');
+
+var routes = {
+  "/api/parsetime": function(parsedUrl) {
+    d = new Date(parsedUrl.query.iso);
+    return {
+      hour: d.getHours(),
+      minute: d.getMinutes(),
+      second: d.getSeconds()
+    };
+  },
+  "/api/unixtime": function(parsedUrl) {
+    return {unixtime: (new Date(parsedUrl.query.iso)).getTime()};
+  }
+}
+
+server = http.createServer(function(request, response) {
+  parsedUrl = url.parse(request.url, true);
+  resource = routes[parsedUrl.pathname];
+  if (resource) {
+    response.writeHead(200, {"Content-Type": "application/json"});
+    response.end(JSON.stringify(resource(parsedUrl)));
+  }
+  else {
+    response.writeHead(404);
+    response.end();
+  }
+});
+server.listen(process.argv[2]);
+*/
