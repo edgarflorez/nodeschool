edd365d6ee71f8f87628f119a851cbe208bc02f7
edgarflorez <edgarfmail@gmail.com>
2018-08-24 23:01:13 -0500

    Functional * Every Some solution

--- functional-javascript/05-every-some.js
+++ functional-javascript/05-every-some.js
@@ -1,7 +1,33 @@
 function checkUsersValid(goodUsers) {
     return function allUsersValid(submittedUsers) {
-      console.log(submittedUsers,'********');
+      
+      return submittedUsers.every(function(submittedUser) {
+        return goodUsers.some(function(goodUser) {
+          return goodUser.id === submittedUser.id
+        })
+      })
+
     };
   }
 
 module.exports = checkUsersValid
+
+
+
+
+
+
+
+/*
+// Official solution
+
+module.exports = function checkUsersValid(goodUsers) {
+  return function allUsersValid(submittedUsers) {
+    return submittedUsers.every(function(submittedUser) {
+      return goodUsers.some(function(goodUser) {
+        return goodUser.id === submittedUser.id
+      })
+    })
+  }
+}
+*/
