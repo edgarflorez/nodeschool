4861a9d8fd5a4d2ac75c278b841713b885718cc1
edgarflorez <edgarfmail@gmail.com>
2018-10-25 00:04:27 -0500

    Functional * Partial Application

---
+++ functional-javascript/09-partial-application.js
@@ -0,0 +1,28 @@
+var slice = Array.prototype.slice;
+
+function logger(namespace) {
+    return function () {
+        console.log.apply( console, [namespace].concat(slice.call(arguments)) );
+    }
+}
+
+module.exports = logger;
+
+
+
+
+
+
+
+/*
+// 
+var slice = Array.prototype.slice
+
+function logger(namespace) {
+    return function() {
+    console.log.apply(console, [namespace].concat(slice.call(arguments)))
+    }
+}
+
+module.exports = logger
+*/
