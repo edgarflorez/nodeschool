2b82fbfa23f9691f9dcd997255b49827503a06b7
Edgar Florez <eflorez@hugeinc.com>
2018-11-03 00:11:55 -0500

    Functional * Blocking event loop

---
+++ functional-javascript/13-blocking-event-loop.js
@@ -0,0 +1,43 @@
+function repeat(operation, num) {
+  // modify this so it can be interrupted
+  if (num <= 0) return
+  operation();
+  if(num%10 ===0){
+    setTimeout(() => {
+      repeat(operation, --num)
+    });
+
+  }else {
+    repeat(operation, --num)
+  }
+}
+
+module.exports = repeat
+
+
+
+
+
+
+
+
+/*
+function repeat(operation, num) {
+  if (num <= 0) return
+
+  operation()
+
+  // release control every 10 or so
+  // iterations.
+  // 10 is arbitrary.
+  if (num % 10 === 0) {
+    setTimeout(function() {
+      repeat(operation, --num)
+    })
+  } else {
+    repeat(operation, --num)
+  }
+}
+
+module.exports = repeat
+*/
