2fecd8bb48cbd191856ed68521377cb4f7928874
Edgar Florez <eflorez@hugeinc.com>
2018-11-05 18:33:12 -0500

    Functional * Recursion

---
+++ functional-javascript/16-recursion.js
@@ -0,0 +1,18 @@
+module.exports = function getDependencies(tree) {
+  // SOLUTION GOES HERE
+  // Note: Feel free to add additional arguments
+  // to this function for use with recursive calls.
+  // Or not! There are many ways to recurse.
+  //console.log(tree);
+  if (typeof tree['dependencies'] != 'undefined' ){
+    return getDependencies(tree['dependencies']);
+  }
+  let result  = []
+  return result.concat( Object.keys(tree).forEach( function (value) {
+    result.push(value + '@' + tree[value]['version'] );
+  } )).sort();
+
+  //console.log(result);
+
+
+}
