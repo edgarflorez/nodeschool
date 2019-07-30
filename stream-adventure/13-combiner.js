b04d0693e0c3b69962e842bc9f4c1f641bc56056
edgarflorez <edgarfmail@gmail.com>
2019-07-30 06:11:13 -0500

    Stream Adventure * Combiner progress

--- stream-adventure/13-combiner.js
+++ stream-adventure/13-combiner.js
@@ -4,13 +4,31 @@ const through = require("through2").obj;
 const zlib = require("zlib");
 
 module.exports = function() {
+  let input = through(write, end);
+  /*
   let books = {};
   let input = through(write, end);
   let currentGenre;
   let booksByGenre;
   let counter = 0;
+  */
+
+  let books = {};
+  let currentGenre;
 
   function write(buf, _, next) {
+    // this.push(buf.toString().toUpperCase());
+    buf = JSON.parse(buf);
+    if (buf.type == "genre") {
+      currentGenre = buf.name;
+      books[currentGenre] = [];
+    } else {
+      books[currentGenre] = books[currentGenre].concat([buf.name]);
+    }
+    // this.push(JSON.stringify(books));
+    console.log("------------");
+    next();
+    /*
     buf = JSON.parse(buf);
     console.log(buf);
     if (buf.type == "genre") {
@@ -23,15 +41,21 @@ module.exports = function() {
     }
     // counts[buf.country] = (counts[buf.country] || 0) + 1;
     next();
+    */
   }
 
   function end(done) {
-    console.log("--", books);
-    // counter.setCounts(counts);
-    done();
+    // this.push(books);
+    console.log("***********************");
+    // console.log(this);
+    return done();
+  }
+  function flush(callback) {
+    this.push(this.total);
+    return callback();
   }
 
-  return combine(input, zlib.createGzip(books), process.stdout);
+  return combine(split(), input, process.stdout);
   // read newline-separated json
   // group books into genres,
   // then gzip the output
